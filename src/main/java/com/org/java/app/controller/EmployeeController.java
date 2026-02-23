19881962fef313e94bceaabf0e2c8fcc4f00721a
javatechivkp <sreenivasarao.javasoft9@gmail.com>
2026-02-23 15:23:08 +0530

    REWETTE

--- src/main/java/com/org/java/app/controller/EmployeeController.java
+++ src/main/java/com/org/java/app/controller/EmployeeController.java
@@ -48,7 +48,7 @@ public class EmployeeController {
 	
 	@GetMapping("/welcome")
 	public String welcomeTest() {
-		return "WELCOME TO AWS TEST CI/CD AUTOMATION PERFECTLLY WORKING FINE";
+		return "WELCOME TO AWS TEST CI/CD AUTOMATION PERFECTLLY WORKING FINE 1 2 3 LOADING";
 	}
 
 	@PostMapping("/save")
